dec34e836c2e5f950e43bc5c804801ce7fd0b19c
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-01-16 16:08:15 +0100

    ADD: edit

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -60,9 +60,9 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Project $project)
     {
-        //
+        return view('admin.projects.edit', compact('project'));
     }
 
     /**
@@ -72,9 +72,12 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Project $project)
     {
-        //
+        $formData = $request->all();
+        $formData['slug'] = Project::generateSlug($formData['title']);
+        $project->update($formData);
+        return redirect()->route('admin.projects.index');
     }
 
     /**
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>PROVA</h1>
+    <div class="container">
+        <h2 class="text-center">Modify Project</h2>
+        <div class="row justify-content-center">
+            <div class="col-8">
+                {{-- @include('partials.errors') --}}
+                <form action="{{ route('admin.projects.update', $project->slug) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-3">
+                        <label for="title">Title</label>
+                        <input type="text" id="title" name="title" class="form-control"
+                            value="{{ old('title', $project->title) }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description">Description</label>
+                        <textarea name="description" id="description" rows="10" class="form-control">{{ old('description', $project->description) }}</textarea>
+                    </div>
+                    <button class="btn btn-success" type="submit">Save</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
