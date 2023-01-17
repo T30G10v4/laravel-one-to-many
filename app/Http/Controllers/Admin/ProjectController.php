8f655dcc4a96df89c853de7c6e77576414f3d828
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-01-17 18:03:29 +0100

    ERROR: no record in db

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -26,7 +27,9 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $types = Type::all();
+
+        return view('admin.projects.create', compact('types'));
     }
 
     /**
@@ -38,6 +41,7 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         $formData = $request->all();
+
         $formData['slug'] = Project::generateSlug($formData['title']);
         $project = Project::create($formData);
         return redirect()->route('admin.projects.index');
--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -12,6 +12,18 @@
                         <label for="title">Title</label>
                         <input type="text" id="title" name="title" class="form-control" value="{{ old('title') }}">
                     </div>
+
+                    <div class="mb-3">
+                        <label for="type">Tipology</label>
+                        <select name="type_id" id="type" class="form-select">
+                            <option value="">Select</option>
+                            @foreach ($types as $type)
+                                <option value="{{ $type->id }}">{{ $type->name }}</option>
+                            @endforeach
+                        </select>
+
+                    </div>
+
                     <div class="mb-3">
                         <label for="description">Description</label>
                         <textarea name="description" id="description" rows="10" class="form-control">{{ old('description') }}</textarea>
